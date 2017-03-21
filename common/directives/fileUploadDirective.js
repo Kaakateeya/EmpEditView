a7c919847d3b4ddab8b00f7127c19021f259d36c
umakusumuru <kusumuruuma@gmail.com>
2017-03-21 17:02:15 +0530

    done emp edits fff

--- common/directives/fileUploadDirective.js
+++ common/directives/fileUploadDirective.js
@@ -1,31 +1,31 @@
-(function() {
-    'use strict';
+// (function() {
+//     'use strict';
 
-    angular
-        .module('KaakateeyaEmpEdit')
-        .directive('fileModel', directive);
-    directive.$inject = ['$parse'];
+//     angular
+//         .module('KaakateeyaEmpEdit')
+//         .directive('fileModel', directive);
+//     directive.$inject = ['$parse'];
 
-    function directive($parse) {
+//     function directive($parse) {
 
-        var directive = {
-            link: link,
-            restrict: 'A'
-        };
-        return directive;
+//         var directive = {
+//             link: link,
+//             restrict: 'A'
+//         };
+//         return directive;
 
-        function link(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
+//         function link(scope, element, attrs) {
+//             var model = $parse(attrs.fileModel);
+//             var modelSetter = model.assign;
 
-            element.bind('change', function() {
-                scope.$apply(function() {
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
+//             element.bind('change', function() {
+//                 scope.$apply(function() {
+//                     modelSetter(scope, element[0].files[0]);
+//                 });
+//             });
 
 
-        }
-    }
+//         }
+//     }
 
-})();
+// })();
