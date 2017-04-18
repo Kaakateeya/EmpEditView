a77c9199a6c49e039c4b4f092501a26f1dacac4c
lakshmidevi <lakshmidevi407@gmail.com>
2017-04-18 12:51:24 +0530

    menu color

--- base/controller/basectrl.js
+++ base/controller/basectrl.js
@@ -18,21 +18,21 @@
          };
          scope.backgroundcolor = function(status) {
              console.log(status);
-             var color = "{'background':'#EFEFEF'}";
+             var color = '#EFEFEF';
              switch (status) {
                  case 54:
-                     color = "{'background':'#EFEFEF'}";
+                     color = '#EFEFEF';
                      break;
                  case 55:
-                     color = "{'background': '#185615'}";
+                     color = '#185615';
                      break;
                  case 56:
                  case 394:
-                     color = "{'background':'#BCC3BE'}";
+                     color = '#BCC3BE';
                      break;
                  case 57:
                  case 393:
-                     color = "{'background':'#17F067'}";
+                     color = '#17F067';
                      break;
              }
              return color;
